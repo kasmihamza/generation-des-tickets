262f7bb57ebe2bbd20d641fb953def210ac790ec
Hamza Kasmi <hkasmi89@gmail.com>
2023-09-02 13:20:23 +0100

    add the project backend

---
+++ GenerationDesTickets (1)/GenerationDesTickets/src/main/java/com/example/GenerationDesTickets/GenerationDesTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.example.GenerationDesTickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerationDesTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenerationDesTicketsApplication.class, args);
+	}
+
+}
---
+++ GenerationDesTickets (1)/GenerationDesTickets/src/test/java/com/example/GenerationDesTickets/GenerationDesTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GenerationDesTickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerationDesTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
